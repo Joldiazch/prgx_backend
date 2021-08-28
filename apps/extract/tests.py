feadc8d7304ab53f2eee0068003927781bb04a39
jose luis diaz <jluis.diaz@udea.edu.co>
2021-08-28 04:57:21 -0500

    create new test for db-data enspoint

--- apps/extract/tests.py
+++ apps/extract/tests.py
@@ -66,3 +66,12 @@ class ExtractApiTestCase(TestCase):
         self.assertEqual(data.get('start_date'), '10/10/2021')
         self.assertEqual(data.get('end_date'), '10/10/2022')
         self.assertEqual(data.get('comments'), 'CRAS ACCUMSAN AC PURUS ET ALIQUAM. PHASELLUS EGET CURSUS FELIS. VIVAMUS TINCIDUNT IACULIS PURUS\nAT IMPERIAL. INTEGER EGRET DUI EULIMID, DAP BUS VALET VEL, INTERDORM IPSUM.')
+
+
+    def test_db_data_endpoint(self):
+        """
+        test if /db-data/ endpoint is work
+        """
+        url = reverse('db_data')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
